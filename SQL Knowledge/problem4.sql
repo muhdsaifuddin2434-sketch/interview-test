407fa47fc1505508a657adef75d6c9937c8e255c
muhdsaifuddin2434-sketch <muhdsaifuddin2434@gmail.com>
2025-12-17 15:28:03 +0800

    Delete SQL Knowledge directory

--- SQL Knowledge/problem4.sql
+++
@@ -1,7 +0,0 @@
-UPDATE inventories
-SET price = 900.00
-WHERE product_id = 2;
-
-
-UPDATE inventories
-SET price = price * 1.10;
--- SQL Knowledge/problem6.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-name,
-position
-FROM
-employees
-WHERE
-salary > 50000;
